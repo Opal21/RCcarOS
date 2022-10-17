1a4046522f16c641802165c87a24a6430b9e8dfe
TomeSneaker <115901627+TomeSneaker@users.noreply.github.com>
2022-10-17 22:49:56 +0200

    Create flaskcalling.py

---
+++ flaskcalling.py
@@ -0,0 +1,31 @@
+#muszę jeszcze zrobić research i poświęcić więcej czasu.
+
+# import main Flask class and request object
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    return 'Query String Example'
+
+@app.route('/form-example')
+def form_example():
+    return 'Form Data Example'
+
+@app.route('/json-example')
+def json_example():
+    return 'JSON Object Example'
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
+    
+    
+@app.route('/query-example2')
+def query_example2():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
