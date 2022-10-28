5ed69a8eb65edfb0ad943429d8bfe616b0fbbdff
TomeSneaker <115901627+TomeSneaker@users.noreply.github.com>
2022-10-28 17:47:21 +0200

    Update websiteConnector.py

--- websiteConnector.py
+++ websiteConnector.py
@@ -1,31 +1,34 @@
+from flask import Flask, render_template, Response
+import cv2
 
-
-# import main Flask class and request object
-from flask import Flask, request
-
-# create the Flask app
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
-@app.route('/query-example')
-def query_example():
-    return 'Query String Example'
 
-@app.route('/form-example')
-def form_example():
-    return 'Form Data Example'
+def generate_frames():
+    while True:
 
-@app.route('/json-example')
-def json_example():
-    return 'JSON Object Example'
+        ## read the camera frame
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
 
-if __name__ == '__main__':
-    # run app in debug mode on port 5000
-    app.run(debug=True, port=5000)
-    
-    
-@app.route('/query-example2')
-def query_example2():
-    # if key doesn't exist, returns None
-    language = request.args.get('language')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') #generator of frames
 
-    return '''<h1>The language value is: {}</h1>'''.format(language)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
