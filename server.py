bd0b7dbc92a4323fdeeeb7b3d5a42ae190dd156f
aq4rius <96205945+aq4rius@users.noreply.github.com>
2022-11-08 19:24:54 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template,Response, session
+from flask_socketio import SocketIO
+import cv2
+
+
+
+async_mode = None
+app = Flask(__name__)
+socket_ = SocketIO(app, async_mode=async_mode)
+
+
+camera=cv2.VideoCapture(0)
+def generate_frames():
+    while True:
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+## listeners to the client requests
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    print(message)
+
+@socket_.on('custom-event1', namespace='/test')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socket_.on('custom-event2', namespace='/test')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socket_.on('custom-event3', namespace='/test')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socket_.on('custom-event4', namespace='/test')
+def handle_message(data):
+    print('received message: ' + data)
+
+@socket_.on('custom-event5', namespace='/test')
+def handle_message(data):
+    print('received message: ' + data)
+
+if __name__ == '__main__':
+    socket_.run(app, debug=True)
