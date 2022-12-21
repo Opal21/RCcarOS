437ce41b11d17477acdb660c548f9ad96d06bf44
aq4rius <96205945+aq4rius@users.noreply.github.com>
2022-12-21 22:06:36 +0100

    Update server.py

--- server.py
+++ server.py
@@ -39,38 +39,29 @@ pwm_front_right.start(motorDriver.INITIAL_PWM_DUTY_CYCLE)
 pwm_rear_right = GPIO.PWM(motorDriver.REAR_RIGHT_MOTOR_ENABLE_PIN, motorDriver.PWM_FREQUENCY)
 pwm_rear_right.start(motorDriver.INITIAL_PWM_DUTY_CYCLE)
 
-camera = cv2.VideoCapture(0)
-
-
+camera=cv2.VideoCapture(0)
 def generate_frames():
     while True:
-
-        # Read the frame
-        _, frame = camera.read()
-
-        # Show the frame
-        cv2.imshow("Live Feed", frame)
-
-        # Return the frame
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-        # Stop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
             break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
 
-    # Release the capture
-    camera.release()
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socket_.async_mode)
 
-
 @app.route('/video')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 # listeners to the client requests
