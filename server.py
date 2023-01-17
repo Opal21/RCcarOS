4bd201c9c9777e0d1e6cd43e7d18f9c6941f441d
Marcin Opalinski <marcinopalinskito@gmail.com>
2023-01-17 08:16:15 +0100

    Code style fixes

--- server.py
+++ server.py
@@ -39,29 +39,31 @@ pwm_front_right.start(motorDriver.INITIAL_PWM_DUTY_CYCLE)
 pwm_rear_right = GPIO.PWM(motorDriver.REAR_RIGHT_MOTOR_ENABLE_PIN, motorDriver.PWM_FREQUENCY)
 pwm_rear_right.start(motorDriver.INITIAL_PWM_DUTY_CYCLE)
 
-camera=cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
+
+
 def generate_frames():
     while True:
-            
-        ## read the camera frame
-        success,frame=camera.read()
+        # read the camera frame
+        success, frame = camera.read()
         if not success:
             break
         else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
 
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socket_.async_mode)
 
+
 @app.route('/video')
 def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 # listeners to the client requests
